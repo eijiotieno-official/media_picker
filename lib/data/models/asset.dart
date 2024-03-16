f6992d61fa7d8cf93b621f1248fd6725a13082f3
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-16 21:13:53 +0300

    asset model

---
+++ lib/data/models/asset.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+class Asset {
+  final AssetEntity assetEntity;
+  final Widget widget;
+  Asset({
+    required this.assetEntity,
+    required this.widget,
+  });
+}
