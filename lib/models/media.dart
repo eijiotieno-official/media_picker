a379edd4a61d3450debf2d84118fd28d721dfc24
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-18 17:14:08 +0300

    media model

--- lib/models/media.dart
+++ lib/models/media.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:photo_manager/photo_manager.dart';
 
+// Define a class to hold media assets and corresponding widgets
 class Media {
-  final AssetEntity assetEntity;
-  final Widget widget;
+  final AssetEntity
+      assetEntity; // Represents a media asset managed by photo_manager
+  final Widget widget; // Represents a Flutter widget associated with the asset
   Media({
-    required this.assetEntity,
-    required this.widget,
+    required this.assetEntity, // Initialize with a required AssetEntity
+    required this.widget, // Initialize with a required Widget
   });
 }
