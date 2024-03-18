b936e4b8c31f3b3fe9c74ca7a653fd9da4a3f23d
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-18 17:17:04 +0300

    fetch medias

--- lib/services/fetch_medias.dart
+++ lib/services/fetch_medias.dart
@@ -4,30 +4,40 @@ import 'package:photo_manager/photo_manager.dart';
 import 'package:photo_manager_image_provider/photo_manager_image_provider.dart';
 import 'package:transparent_image/transparent_image.dart';
 
+// Function to fetch media items from a specific album and page
 Future<List<Media>> fetchMedias({
-  required AssetPathEntity album,
-  required int page,
+  required AssetPathEntity album, // The album from which to fetch media
+  required int page, // The page number of media to fetch
 }) async {
-  List<Media> medias = [];
+  List<Media> medias = []; // List to hold fetched media items
 
-  final List<AssetEntity> entities =
-      await album.getAssetListPaged(page: page, size: 30);
+  try {
+    // Get a list of asset entities from the specified album and page
+    final List<AssetEntity> entities =
+        await album.getAssetListPaged(page: page, size: 30);
 
-  for (AssetEntity entity in entities) {
-    Media media = Media(
-      assetEntity: entity,
-      widget: FadeInImage(
-        placeholder: MemoryImage(kTransparentImage),
-        fit: BoxFit.cover,
-        image: AssetEntityImageProvider(
-          entity,
-          thumbnailSize: const ThumbnailSize.square(500),
-          isOriginal: false,
+    // Loop through each asset entity and create corresponding Media objects
+    for (AssetEntity entity in entities) {
+      Media media = Media(
+        assetEntity: entity, // Assign the asset entity to the Media object
+        // Create a FadeInImage widget to display the media thumbnail
+        widget: FadeInImage(
+          placeholder: MemoryImage(kTransparentImage), // Placeholder image
+          fit: BoxFit.cover, // Set the fit mode to cover
+          // Use AssetEntityImageProvider to load the media thumbnail
+          image: AssetEntityImageProvider(
+            entity,
+            thumbnailSize: const ThumbnailSize.square(500), // Thumbnail size
+            isOriginal: false, // Load a non-original (thumbnail) image
+          ),
         ),
-      ),
-    );
-    medias.add(media);
+      );
+      medias.add(media); // Add the created Media object to the list
+    }
+  } catch (e) {
+    // Handle any exceptions that occur during fetching
+    debugPrint('Error fetching media: $e');
   }
 
-  return medias;
+  return medias; // Return the list of fetched media items
 }
