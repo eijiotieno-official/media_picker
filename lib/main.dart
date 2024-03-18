eb9817b1beae900c1fafa9abab406b5f0e1a427b
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-18 17:44:41 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:media_picker/ui/screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MainApp()); // Entry point of the application
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key}); // Constructor for MainApp widget
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: HomeScreen(), // Set HomeScreen as the home page of the MaterialApp
     );
   }
 }
