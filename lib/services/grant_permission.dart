b8eebf94b7ea1cb8a81b0e4a6b06852001ef7a15
Eiji Otieno <eijiotieno.official.mail@gmail.com>
2024-03-24 12:08:23 +0300

    permissions

---
+++ lib/services/grant_permission.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+// Function to request and handle permissions for accessing videos and photos
+Future<void> grantPermissions() async {
+  try {
+    // Check if permissions are already granted
+    final bool videosGranted = await Permission.videos.isGranted;
+    final bool photosGranted = await Permission.photos.isGranted;
+
+    // If permissions are not granted, request them
+    if (!photosGranted || !videosGranted) {
+      final Map<Permission, PermissionStatus> statuses = await [
+        Permission.videos,
+        Permission.photos,
+      ].request();
+
+      // If permissions are permanently denied, open app settings
+      if (statuses[Permission.videos] == PermissionStatus.permanentlyDenied ||
+          statuses[Permission.photos] == PermissionStatus.permanentlyDenied) {
+        // Open app settings to allow users to grant permissions
+        await openAppSettings();
+      }
+    }
+  } catch (e) {
+    // Handle any exceptions that occur during permission handling
+    debugPrint('Error granting permissions: $e');
+  }
+}
